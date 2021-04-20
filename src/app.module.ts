79e4c471ee987ec609b1a9e6b2161288b938bf6b
Karen Gomes <kngs@ic.ufal.br>
2021-04-20 02:03:25 -0300

    refactor: 💡 change db and remove some dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Product } from './product/product.entity';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db',
+    entities: [Product],
+    synchronize: true,
+  }) ],
   controllers: [AppController],
   providers: [AppService],
 })
