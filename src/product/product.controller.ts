d0ccb5108e1f92a79828ef6542342d23c5925d02
Karen Gomes <kngs@ic.ufal.br>
2021-04-20 02:03:52 -0300

    feat: 🎸 add routes in controller and service

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,15 +1,45 @@
-import { Controller } from '@nestjs/common';
-import { Crud } from '@nestjsx/crud';
-import { Product } from './product';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Product } from './product.entity';
 import { ProductService } from './product.service';
 
-@Crud({
-  model: {
-    type: Product,
-  },
-})
 @Controller('products')
-
 export class ProductController {
-    constructor(public service: ProductService) {}
+  constructor(public productService: ProductService) {}
+
+  @Get()
+  async getProduct(): Promise<Product[]> {
+    return this.productService.findAll();
+  }
+
+  @Get(':id')
+  async getProductById(@Param('id') id): Promise<Product> {
+    const products = await this.productService.findAll();
+
+    return products.find((product) => product.id === Number(id));
+  }
+
+  @Post()
+  async create(@Body() newProduct: Product): Promise<any> {
+    return this.productService.create(newProduct);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id, @Body() productData: Product): Promise<any> {
+    productData.id = Number(id);
+    console.log('Updating #', +productData.id);
+    return this.productService.update(productData);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id): Promise<any> {
+    return this.productService.delete(id);
+  }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from "@nestjs/typeorm";
-import { TypeOrmCrudService } from "@nestjsx/crud-typeorm";
-import { Product } from './product';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { Product } from './product.entity';
 
 @Injectable()
-export class ProductService extends TypeOrmCrudService<Product> {
-    constructor(@InjectRepository(Product) repo) {
-        super(repo);
-    }
+export class ProductService {
+  constructor(
+    @InjectRepository(Product) private productRepository: Repository<Product>,
+  ) {}
+
+  async findAll(): Promise<Product[]> {
+    return await this.productRepository.find();
+  }
+
+  async create(Product: Product): Promise<Product> {
+    return await this.productRepository.save(Product);
+  }
+
+  async update(Product: Product): Promise<UpdateResult> {
+    return await this.productRepository.update(Product.id, Product);
+  }
+
+  async delete(id): Promise<DeleteResult> {
+    return await this.productRepository.delete(id);
+  }
 }
